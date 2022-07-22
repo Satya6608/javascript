63b537d08aa12859347f72a33ec9976e657f0cb8
satya <satyaprakash6608@gmail.com>
2022-07-22 12:46:43 +0530

    new files to learn more

---
+++ basics.js
@@ -0,0 +1,143 @@
+//console.log("hello world");
+/*var r = 10;
+var area = 3.14*r*r;
+console.log(`
+     radius of circle = ${r};
+     area of circle = ${area};
+`)*/
+/*
+var r = Number(prompt("Please enter the radius of circle"));
+var area = 3.14*r*r;
+alert(`
+     radius of circle = ${r};
+     area of circle = ${area};
+`)
+document.write(`
+radius of circle = ${r}</br>
+area of circle = ${area}
+`)
+*/
+
+// wap to print area of racctagle 
+/*
+var l = 50;
+var w = 80;
+var h =50
+var area = 2*(l*w+l*h+h*w);
+
+console.log(`
+     lenght of rectangle = ${l};
+     width of ractangle = ${b}
+     area of the rectangle = ${area}
+`)
+document.write(`
+     lenght of rectangle = ${l}</br>
+
+     width of ractangle = ${b}</br>
+     
+     area of the rectangle = ${area}
+`)
+document.write(`
+     lenght of cuboid = ${l}</br>
+
+     width of cuboid = ${w}</br>
+     height of cuboid = ${h}</br>
+     
+     area of the cuboid = ${area}
+`)
+*/
+
+//wap to print area of right angle triangle 
+
+
+/*var base = Number(prompt("Enter base of triangle"));
+var height = Number(prompt("Enter height of triangle"));
+var area = height*base;
+document.write(`
+     base of triangle = ${base}</br>
+
+     height of triangle = ${height}</br>
+     
+     area of the triangle = ${area}
+`)
+alert(`
+     base of triangle = ${base}
+
+     height of triangle = ${height}
+     
+     area of the triangle = ${area}
+`)*/
+
+//wap to print area of square
+/*
+var side = Number(prompt("Enter base of triangle"));
+var area = side*side;
+document.write(`
+   
+     side of Square = ${side}</br>
+     
+     area of the Square = ${area}
+`)
+alert(`
+    side  of Square = ${side}
+     
+     area of the Square = ${area}
+`)
+*/
+
+
+//wap to swap two variables
+/*
+var a =Number(prompt("enter the first no."))
+var b =Number(prompt("enter the second no."))
+var t = null
+
+      t=a
+      a=b;
+      b=t;
+      alert(`
+           first no after swap =${a}
+           second no after swap =${b};
+      `)*/
+
+      // wap to conver c temprature c to f
+
+
+/*
+var c =Number(prompt("enter the temprature in celcius."))
+
+var f =  (c*9/5)+32;
+
+      
+      alert(`
+      the temprature in celcius. =${c};
+      the temprature in feranheight. =${f};
+      `)
+var a =Number(prompt("enter the temprature in feranheight."))
+
+var b =  (f-32)*5/9;
+
+      
+      alert(`
+      the temprature in feranheight. =${a};
+      the temprature in celcius. =${b};
+      `)
+      */
+var c =-40;
+
+var f =  (c*9/5)+32;
+
+      
+      console.log(`
+      the temprature in celcius. =${c};
+      the temprature in feranheight. =${f};
+      `)
+var a =-40
+
+var b =  (f-32)*5/9;
+
+      
+      console.log(`
+      the temprature in feranheight. =${a};
+      the temprature in celcius. =${b};
+      `)
